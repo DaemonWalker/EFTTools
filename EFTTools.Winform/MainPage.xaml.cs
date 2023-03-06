9cada2917ed35528de5095bf07046ad0c755803c
ccdd <215136855@qq.com>
2023-03-06 21:40:45 +0800

    删除MAUI工程

--- EFTTools.Winform/MainPage.xaml.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EFTTools.Winform {
-    public partial class MainPage : ContentPage {
-        public MainPage() {
-            InitializeComponent();
-        }
-    }
-}
--- EFTTools.Winform/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace EFTTools.Winform {
-    [Register("AppDelegate")]
-    public class AppDelegate : MauiUIApplicationDelegate {
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    }
-}
